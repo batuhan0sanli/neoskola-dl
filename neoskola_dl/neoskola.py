7751902dfb4aa27f478064479a706597561fe97e
Batuhan Şanlı <batuhansanli@gmail.com>
2022-09-29 00:48:55 +0300

    add main neoskola

---
+++ neoskola_dl/neoskola.py
@@ -0,0 +1,12 @@
+from neoskola_dl.auth import Auth
+from neoskola_dl.items.course import Course
+
+
+class NeoSkola:
+    def __init__(self, token=None, email=None, password=None):
+        self.auth = Auth(token=token, email=email, password=password)
+        self.auth.login()
+
+    def download_course(self, course_url):
+        course = Course(course_url, self.auth)
+        course.download()
