8c0d9ecbaa9226a49232bf1e2e3af7f3bf1e899d
Batuhan Şanlı <batuhansanli@gmail.com>
2022-09-29 00:47:07 +0300

    add config

---
+++ neoskola_dl/config.py
@@ -0,0 +1,10 @@
+import os
+
+# Path to the directory where the downloaded files will be saved
+# If the directory does not exist, it will be created
+# If the directory is not specified, the current directory will be used
+# DOWNLOAD_PATH = os.path.join(os.path.expanduser('~'), 'Downloads')
+# DOWNLOAD_PATH = os.path.join(os.path.expanduser('~'), 'Downloads', 'Neoskola')
+DOWNLOAD_PATH = "./Downloads"
+CHUNK_SIZE = 256 * 1024  # 256 KB
+DOWNLOAD_OPTION = ['1080p', 'mp3']  # '1080p' or '720p' or '540p' or '240p' or 'mp3'
